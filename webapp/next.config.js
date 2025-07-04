47d5596815423097b29bf57048924d488b9d209a
AnkitB47 <jha.ankit230@example.com>
2025-07-04 15:58:23 +0200

    Fastapi routing adaptability

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -7,15 +7,12 @@ const nextConfig = {
     },
   },
   async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8000/:path*',
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
+      },
+    ];
   },
 };
 
